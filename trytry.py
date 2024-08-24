42e5e386c4831d82b93f1563b4f11a0b8af249b2
vicky <vicky05pong@gmail.com>
2024-08-24 19:50:33 +0800

    Nothing

--- trytry.py
+++ trytry.py
@@ -166,83 +166,6 @@ pygame.draw.rect(image_with_frame_surface_1, frame_color_1, (0, 0, image_with_fr
 # Draw the image onto the surface with the frame
 image_with_frame_surface_1.blit(image_1, (frame_thickness_1, frame_thickness_1))
 
-# Image settings for Medince
-image_2 = pygame.image.load('medicine.png') 
-image_2_size = (200, 200)  
-image_2 = pygame.transform.scale(image_2, image_2_size)
-image_2_width, image_2_height = image_2.get_size()
-label_offset = 12
-image_rect_2 = image_2.get_rect(topleft=(390, (screen_height - image_2_height) // 2 + 200))  
-
-
-# Label text with multiple lines
-label_text_lines_2 = [
-    "  MED Kit: ",
-    "  50% of chance",
-    "  to get heal",
-    "  but also 50%",
-    "  of chance to",
-    "  deduct a life"
-]
-
-font_10 = pygame.font.Font("Anton.ttf", 20)
-
-label_surfaces_2 = [font_10.render(line, True, (WHITE)) for line in label_text_lines_2]
-label_rects_2 = [surf.get_rect(topleft=(image_rect_2.right + label_offset, image_rect_2.top + i * font_10.get_height())) for i, surf in enumerate(label_surfaces_2)]
-
-# Frame settings
-frame_thickness_2 = 10
-frame_color_2 = BLACK
-background_color_2 = DARKGREY 
-
-# Image with frame surface
-image_with_frame_surface_2 = pygame.Surface((image_2_width + 2 * frame_thickness_2, image_2_height + 2 * frame_thickness_2), pygame.SRCALPHA)
-image_with_frame_surface_2.fill(background_color_2)  
-
-# Draw the border around the image
-pygame.draw.rect(image_with_frame_surface_2, frame_color_2, (0, 0, image_with_frame_surface_2.get_width(), image_with_frame_surface_2.get_height()), frame_thickness_2)
-
-# Draw the image onto the surface with the frame
-image_with_frame_surface_2.blit(image_2, (frame_thickness_2, frame_thickness_2))
-
-# Image settings for Handsaw
-image_3 = pygame.image.load('handsaw.png') 
-image_3_size = (200, 200)  
-image_3 = pygame.transform.scale(image_3, image_3_size)
-image_3_width, image_3_height = image_3.get_size()
-image_rect_3 = image_3.get_rect(topleft=(730, (screen_height - image_3_height) // 2 + 200))  
-
-
-# Label text with multiple lines
-label_text_lines_3 = [
-    "  Handsaw: ",
-    "  double up the",
-    "  damage of the",
-    "  guns, high rick",
-    "  high reward",
-    "  "
-]
-
-font_10 = pygame.font.Font("Anton.ttf", 20)
-
-label_surfaces_3 = [font_10.render(line, True, (WHITE)) for line in label_text_lines_3]
-label_rects_3 = [surf.get_rect(topleft=(image_rect_3.right + label_offset, image_rect_3.top + i * font_10.get_height())) for i, surf in enumerate(label_surfaces_3)]
-
-# Frame settings
-frame_thickness_3 = 10
-frame_color_3 = BLACK
-background_color_3 = DARKGREY   
-
-# Image with frame surface
-image_with_frame_surface_3 = pygame.Surface((image_3_width + 2 * frame_thickness_3, image_3_height + 2 * frame_thickness_3), pygame.SRCALPHA)
-image_with_frame_surface_3.fill(background_color_3)  
-
-# Draw the border around the image
-pygame.draw.rect(image_with_frame_surface_3, frame_color_3, (0, 0, image_with_frame_surface_3.get_width(), image_with_frame_surface_3.get_height()), frame_thickness_3)
-
-# Draw the image onto the surface with the frame
-image_with_frame_surface_3.blit(image_3, (frame_thickness_3, frame_thickness_3))
-
 # Define screen states
 SCREEN_MAIN = 0
 SCREEN_PLAY = 1
@@ -514,9 +437,6 @@ while running:
         screen.blit(how_text2_surface, (how_text2_x, how_text2_y))
         screen.blit(text_4_surface, (text_4_button_x, text_4_button_y))  
         screen.blit(image_with_frame_surface_1, (image_rect_1)) 
-
-        screen.blit(image_with_frame_surface_2, (image_rect_2)) 
-        screen.blit(image_with_frame_surface_3, (image_rect_3)) 
         create_rounded_speech_bubble("The game consists of three rounds. At the start of the round the dealer loads the shotgun with a certain amount of red live shells and grey blanks shells in random order. Players then ask to choose either to shoot the dealer or themselves. Depending on whether the player chooses to shoot themselves or the dealer, if the shell is live then either the dealer or the player will lose a life. Each player has a certain amount of life depending on the round. At the first two round you will be save by defibrillators, at the third round where everything gets serious defibrillators will be cut off no more waking up.  Starting on round 2, a set of items will be distributed to you and the dealer. Every item will give you a different advantage.  2 items will be given in round 2 and 4 in round 3.",
         player_x + 50 , player_y -100 , width=800, height=250)
 
@@ -575,16 +495,6 @@ while running:
     if image_rect_1.colliderect(mouse_rect):
             for label_surf, label_rect in zip(label_surfaces, label_rects):
                 screen.blit(label_surf, label_rect)
-
-        # Check collision with image_rect_2
-    if image_rect_2.colliderect(mouse_rect):
-            for label_surf, label_rect_2 in zip(label_surfaces_2, label_rects_2):
-                screen.blit(label_surf, label_rect_2)
-
-        # Check collision with image_rect_3
-    if image_rect_3.colliderect(mouse_rect):
-            for label_surf, label_rect_3 in zip(label_surfaces_3, label_rects_3):
-                screen.blit(label_surf, label_rect_3)
 
 
     pygame.display.flip()
